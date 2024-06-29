7995bbe3e1c2c87465e408d4873309e034af1f1d
Gromp1k <kacperducin@gmail.com>
2024-06-29 21:03:59 +0200

    renamed config to settings, added logging config

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import configparser
+from logging.config import dictConfig
+import pathlib
 import logging
-import discord
-from discord.ext import commands
 
 # Read properties file
 config = configparser.ConfigParser()
@@ -45,9 +45,47 @@ MESSAGE_REMINDER_NOTE = (
 
 PARTICIPANTS_LIMIT = 21
 
+BASE_DIR = pathlib.Path(__file__).parent
+CMDS_DIR = BASE_DIR / "cmds"
+COGS_DIR = BASE_DIR / "cogs"
 
 
-# Logging level
-logging.basicConfig(level=logging.INFO)
+LOGGING_CONFIG = {
+    "version": 1,
+    "disabled_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)-10s - %(asctime)s - %(module)-15s : %(message)s"
+        },
+        "standard": {"format": "%(levelname)-10s - %(name)-15s : %(message)s"},
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+        "console2": {
+            "level": "WARNING",
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "logs/infos.log",
+            "mode": "w",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "bot": {"handlers": ["console"], "level": "INFO", "propagate": False},
+        "discord": {
+            "handlers": ["console2", "file"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
 
-print("Config init")
+dictConfig(LOGGING_CONFIG)
